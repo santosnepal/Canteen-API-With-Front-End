9849b25c2f826fffb5073a17882dff2c082cba53
santosnepal <santoshnepal608@gmail.com>
2022-02-21 11:14:54 +0545

    File showld be included validation done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,13 @@
 const UserService = require('../services/user.service');
+const ValidationException = require('../exceptions/validationException');
 const bcrypt = require('bcrypt');
 class UserController{
     async create(req,res,next){
         try {
             const userData = req.body;
+            if (!req.file) {
+                return next(new ValidationException(false,'Missing Profile Picture Image',null))
+            }
             userData.profile_pic = req.file.path;
             const password = await bcrypt.hash(userData.password,10);
             userData.password = password;
