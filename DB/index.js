febab40455f7737a8ba1e480cc9e1a7f8f2b3818
santosnepal <santoshnepal608@gmail.com>
2022-02-24 07:40:10 +0545

    Relation bettween credit and user and credit and item maintained

--- DB/index.js
+++ DB/index.js
@@ -1,41 +1,42 @@
-const Sequelize = require('sequelize');
-const users = require('../Models/users.model');
-const roles = require('../Models/roles.model');
-const users_roles = require('../Models/user_role.model');
-const category = require('../Models/category.model');
-const item = require('../Models/items.model');
-const order = require('../Models/order.model');
-
+const Sequelize = require("sequelize");
+const users = require("../Models/users.model");
+const roles = require("../Models/roles.model");
+const users_roles = require("../Models/user_role.model");
+const category = require("../Models/category.model");
+const item = require("../Models/items.model");
+const order = require("../Models/order.model");
+const credit_account = require("../Models/credit_account.model");
 
 //database configurations
-const DB_USERNAME="postgres";
-const DB_PASSWORD="codecamp";
-const DB_NAME="canteen";
-const DB_HOST="localhost";
-const DB_PORT="5432";
-const DIALECT="postgres";
-const db = {}
-const sequelize = new Sequelize(DB_NAME,DB_USERNAME,DB_PASSWORD,{
-    dialect:DIALECT,
-    host:DB_HOST,
-    port:DB_PORT,
-    pool: {
-        max:20,
-        idle:30000,
-        min:5
-    },
-    define:{
-        underscored:true
-    }
+const DB_USERNAME = "postgres";
+const DB_PASSWORD = "codecamp";
+const DB_NAME = "canteen";
+const DB_HOST = "localhost";
+const DB_PORT = "5432";
+const DIALECT = "postgres";
+const db = {};
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  dialect: DIALECT,
+  host: DB_HOST,
+  port: DB_PORT,
+  pool: {
+    max: 20,
+    idle: 30000,
+    min: 5,
+  },
+  define: {
+    underscored: true,
+  },
 });
 //making imported model into db model
-const userModel = users(sequelize,Sequelize.DataTypes);
-const roleModel = roles(sequelize,Sequelize.DataTypes);
-const usersrolesModel = users_roles(sequelize,Sequelize.DataTypes);
-const categoryModel = category(sequelize,Sequelize.DataTypes);
-const itemModel = item(sequelize,Sequelize.DataTypes);
-const orderModel = order(sequelize,Sequelize.DataTypes);
-//relation between item and category  category-items one to many 
+const userModel = users(sequelize, Sequelize.DataTypes);
+const roleModel = roles(sequelize, Sequelize.DataTypes);
+const usersrolesModel = users_roles(sequelize, Sequelize.DataTypes);
+const categoryModel = category(sequelize, Sequelize.DataTypes);
+const itemModel = item(sequelize, Sequelize.DataTypes);
+const orderModel = order(sequelize, Sequelize.DataTypes);
+const creditAccountModel = credit_account(sequelize, Sequelize.DataTypes);
+//relation between item and category  category-items one to many
 categoryModel.hasMany(itemModel);
 itemModel.belongsTo(categoryModel);
 //relationship between user and order
@@ -44,9 +45,15 @@ orderModel.belongsTo(userModel);
 //relationship between order and item
 itemModel.hasMany(orderModel);
 orderModel.belongsTo(itemModel);
-// defining many to many relation between user and role 
-userModel.belongsToMany(roleModel,{through:usersrolesModel});
-roleModel.belongsToMany(userModel,{through:usersrolesModel});
+//relation between order and credit_account
+orderModel.hasMany(creditAccountModel);
+creditAccountModel.belongsTo(orderModel);
+//relation between credit_account and user
+userModel.hasMany(creditAccountModel);
+creditAccountModel.belongsTo(userModel);
+// defining many to many relation between user and role
+userModel.belongsToMany(roleModel, { through: usersrolesModel });
+roleModel.belongsToMany(userModel, { through: usersrolesModel });
 db.user = userModel;
 db.role = roleModel;
 db.users_role = usersrolesModel;
@@ -55,4 +62,3 @@ db.item = itemModel;
 db.order = orderModel;
 db.sequelize = sequelize;
 module.exports = db;
-
