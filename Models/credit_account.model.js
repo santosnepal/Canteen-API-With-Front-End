5a70a2e5d81d14c24e231f92947bb5f26984e71a
santosnepal <santoshnepal608@gmail.com>
2022-02-24 07:36:48 +0545

    Created credit account model

---
+++ Models/credit_account.model.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, type) => {
+  return sequelize.define("credit_account", {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+    },
+    user_id: {
+      type: type.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    },
+    order_id: {
+      type: type.INTEGER,
+      references: {
+        model: "orders",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    },
+  });
+};
