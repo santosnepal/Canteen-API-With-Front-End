7dc6fbcce00b03ee45ab9bb4fc205a309df33672
santosnepal <santoshnepal608@gmail.com>
2022-02-25 14:43:55 +0545

    Written paid account controller

---
+++ controllers/paid_account.controller.js
@@ -0,0 +1,34 @@
+const PaidAccountService = require("../services/paid_account.service");
+class PaidAccountController {
+  //saved amount paid by a user only for staff and admin
+  async create(req, res, next) {
+    try {
+      const paidData = req.body;
+      const savedData = await PaidAccountService.create(paidData);
+      res.status(200).json(savedData);
+    } catch (error) {
+      next(error);
+    }
+  }
+  //get all paid amount detail of loginned user
+  async findMyPaidHistory(req, res, next) {
+    try {
+      const whose = req.user.id;
+      const details = await PaidAccountService.findByUserId(whose);
+      res.status(200).json(details);
+    } catch (error) {
+      next(error);
+    }
+  }
+  //get all paid amount detail of a user by admin or staff
+  async findHisPaidHistory(req, res, next) {
+    try {
+      const whose = req.body.userId;
+      const details = await PaidAccountService.findByUserId(whose);
+      res.status(200).json(details);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+module.exports = new PaidAccountController();
